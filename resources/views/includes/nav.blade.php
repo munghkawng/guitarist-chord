5dd733aed630e60d34a45f301a87229f830fe466
MUNG <mung@192.168.0.114>
2022-03-21 01:24:05 +0630

    add logo image

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -2,7 +2,8 @@
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-warning text-decoration-none">
-                Guitarist
+                <img src="{{asset('img/logo.png')}}" alt="" width="50" height="54">
+
             </a>
 
             <ul class="nav nav-pills col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" type="image/x-icon" href="{{asset('img/favicon.png')}}">
     <title>Guitarist Chord | @yield('title')</title>
     {{--font--}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
