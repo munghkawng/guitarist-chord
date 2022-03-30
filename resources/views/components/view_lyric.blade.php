8acd69a903e3001a07e4085d42165ef976dedea2
munghkawng <munghkawngaung3@gmail.com>
2022-03-30 11:05:24 +0630

    add title message

--- resources/views/components/view_lyric.blade.php
+++ resources/views/components/view_lyric.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('title')
-    {{$song->title}}
+    {{ $song->title }} - Guitarist Chord
 @endsection
 @section('content')
     <div class="row">
         <div class="col-md-6">
-            <h4 class="fw-bold"><i class="bi bi-disc-fill"></i> {{$song->title}}</h4>
-            <p><i class="bi bi-mic-fill"></i> Artist:<span class="fw-bold">{{$song->meta['description']}}</span></p>
+            <h4 class="fw-bold"><i class="bi bi-disc-fill"></i> {{ $song->title }}</h4>
+            <p><i class="bi bi-mic-fill"></i> Artist:<span class="fw-bold">{{ $song->meta['description'] }}</span></p>
 
             <div class="song">
 
@@ -15,9 +15,10 @@
             </div>
         </div>
         <div class="col-md-6 embed-responsive embed-responsive-16by9">
-            <iframe width="560" height="315"  class="embed-responsive-item" src="{{$song->meta['canonical_link']}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="560" height="315" class="embed-responsive-item" src="{{ $song->meta['canonical_link'] }}"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
         </div>
     </div>
-
-
 @endsection
