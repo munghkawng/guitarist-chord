40a1e4b129aac9a726ce61f99482ec08cfaad255
munghkawng <munghkawngaung3@gmail.com>
2022-03-30 11:44:27 +0630

    footer change

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="d-flex flex-wrap justify-content-around align-content-center py-3 my-5 border-top">
     <div class="col-md-4 d-flex align-items-center">
-        Copyright &copy; {{ date('Y') }}
+        Copyright &copy; {{ date('Y') }} GuitaristChord
 
     </div>
     <div class="nav col-md-4 justify-content-end list-unstyled d-flex">
