1994fc9d6600e0c143697c5f7857bf38decba7af
munghkawng <munghkawngaung3@gmail.com>
2022-04-23 23:49:11 +0530

    change index page layout

--- resources/views/components/index.blade.php
+++ resources/views/components/index.blade.php
@@ -5,13 +5,22 @@ Guitarist Chord | Myanmar Song Lyrics & Guitar Chords
 @section('content')
 <h5 class="py-3 ms-1 text-uppercase d-flex justify-content-center justify-content-md-start">Trending Lyrics</h5>
 <div class="row">
+
     @foreach ($posts as $post)
-    <div class="col-sm-6 col-md-3 d-flex justify-content-center mb-3">
-        <div class="card" style="width: 18rem;">
+    <div class="col-md-3 mb-3">
+        <div class="card shadow-lg pb-3" style="width: 18rem;">
             <div class="card-body">
-                <h5 class="card-title">{{$post->title}}</h5>
-                <p class="card-text text-muted"><i class="fa-solid fa-guitar"></i> {{$post->meta['description']}}</p>
-                <a href="{{route('lyric.show',$post->slug)}}" class="btn btn-primary">View Lyrics</a>
+                <h5 class="card-title mb-3 fw-bold  text-info">
+                    {{ $post->title }}
+                </h5>
+                <h6 class="card-subtitle mb-2 text-muted"><i class="bi bi-mic-fill"></i> {{ $post->meta['description'] }}
+                </h6>
+
+                <div class="d-grid">
+                    <a href="{{ route('lyric.show', $post->slug) }}" class="btn btn-outline-primary stretched-link"><i class="bi bi-file-music-fill"></i> View
+                        Lyric</a>
+                </div>
+
             </div>
         </div>
     </div>
