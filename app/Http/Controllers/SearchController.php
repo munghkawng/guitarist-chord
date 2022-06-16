70ffb0f9c04634f93bdee4e576b234cfaee8a992
munghkawng <munghkawngaung3@gmail.com>
2022-06-16 23:41:54 +0530

    add some change in search controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,8 +8,12 @@ class SearchController extends Controller
 {
 
     public function search(Request $request){
-        $songs = Post::where('title','like','%'.$request->q.'%')
+        if($request->has('q')){
+             $songs = Post::where('title','like','%'.$request->q.'%')
             ->orWhere('summary','like','%'.$request->q.'%')->get();
+          
+        }
+       
         if(count($songs)){
             return view('components.search',compact('songs'));
 
